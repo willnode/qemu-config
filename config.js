31790da6fefc44a343f3686fcc9882491c56c202
Wildan M <willnode@wellosoft.net>
2026-01-19 18:47:19 +0700

    CPU and Disk dataset

--- config.js
+++ config.js
@@ -1,17 +1,183 @@
-const configData = {
-  architectures: {
-    "x86_64": { binary: "qemu-system-x86_64", machine: "q35", cpu: "host" },
-    "i386": { binary: "qemu-system-i386", machine: "pc", cpu: "pentium3" },
-    "aarch64": { binary: "qemu-system-aarch64", machine: "virt", cpu: "max" },
-    "arm": { binary: "qemu-system-arm", machine: "versatilepb", cpu: "cortex-a15" },
-    "riscv64": { binary: "qemu-system-riscv64", machine: "virt", cpu: "rv64" },
-    "riscv32": { binary: "qemu-system-riscv32", machine: "virt", cpu: "rv32" },
-    "loongarch64": { binary: "qemu-system-loongarch64", machine: "virt", cpu: "la464" }
+
+
+const MACHINES = {
+  // virtualize, fastest, exotic-new
+  "q35": "New PCIe-enabled x86 hardware", // PCI, SCSI
+  // compat, exotic-old
+  "pc": "Old PCI-only x86 hardware", // IDE
+  // legacy, exotic-old
+  "isapc": "Ancient MSDOS-era x86 hardware", // ISA
+  // virtualize
+  "virt": "Virtual",
+}
+
+// https://qemu-project.gitlab.io/qemu/system/qemu-cpu-models.html
+// qemu-system-i386 -cpu help
+
+const CPUS = {
+  // virtualize
+  "max": "Max",
+  "host": "Host (accelerated)",
+  "base": "Baseline",
+  // fastest (x86_64-v4)
+  "EPYC": "2017 AMD EPYC",
+  "Skylake-Server": "2016 Intel Skylake Server",
+  // compat (x86_64-v3)
+  "Skylake-Client": "2016 Intel Skylake",
+  "Broadwell": "2014 Intel Broadwell",
+  //// x86_64-v2
+  "IvyBridge": "2012 Intel IvyBridge",
+  "Westmere": "2010 Intel Core Gen 1",
+  /// x86_64-v1
+  "Penryn": "2007 Intel Core 2 Duo",
+  "Conroe": "2006 Intel Celeron",
+  // legacy
+  "pentium3": "Intel Pentium 3", // i686
+  "pentium": "Intel Pentium Pro", // i586
+  "486": "Intel 80486", // i486
+  // exotic-old
+  "athlon": "AMD Athlon",
+  // exotic-new  
+  "GraniteRapids": "2024 Intel XEON P-Core",
+  "SierraForest": "2024 Intel XEON E-Core",
+  "EPYC-Genoa": "2022 AMD EPYC Genoa",
+}
+
+// qemu-system-x86_64 -device \?
+
+const commonDisks = {
+  // virtualize
+  "virtio-blk": "Virt-IO BLK (basic) disk", // alias to "virtio"
+  "virtio-scsi": "Virt-IO SCSI (advanced) disk",
+  "virtio-9p": "Virt-IO 9P (bridged) disk",
+  "scsi-hd": "virtual SCSI disk",
+  "scsi-cd": "virtual SCSI CD-ROM",
+  // fastest
+  "nvme": "NVME, PCI-based disk",
+  "ufs": "UFS, PCI-based usb stick",
+  "ahci": "ICH9 AHCI, PCI-based disk",
+  // compat
+  "ide-hd": "IDE-based disk",
+  "ide-cd": "IDE-based CDROM",
+  // exotic-old
+  "usb": "USB stick",
+  "sd-card": "SD Card",
+  // exotic-new
+  "am53c974": "AMD Am53c974 PCscsi-PCI SCSI",
+  "dc390": "Tekram DC-390 SCSI adapter",
+  "megasas": "LSI MegaRAID SAS 1078",
+}
+
+const x86Disks = {
+  // legacy
+  "isa-ide": "ISA-based disk",
+  "isa-fdc": "ISA-based floppy disk",
+  "floppy": "Floppy drive",
+  "emmc": "eMMC",
+}
+
+const DISKS = { ...commonDisks, ...x86Disks, }
+
+const DISPLAYS = {}
+
+const NETWORKS = {}
+
+const SOUNDS = {}
+
+const INPUTS = {}
+
+const PRESET_MATRIX = {
+  "virtualize": {
+    index: 0,
+    title: "Virtualize",
+    desc: "Prefer to virtualize devices",
+    machine: ["q35", "virt"],
+    cpu: ["max", "host", "base"],
+    disk: ["virtio-blk", "virtio-scsi", "virtio-9p", "scsi-hd", "scsi-cd"],
   },
-  os_presets: {
-    "debian": { name: "Debian/Ubuntu", vga: "virtio", net: "virtio-net-pci" },
-    "windows": { name: "Windows", vga: "std", net: "e1000", extra: ["-rtc base=localtime"] },
-    "mac": { name: "macOS", vga: "vmware", net: "virtio-net-pci", extra: ["-device isa-applesmc"] }
+  "fastest": {
+    index: 1,
+    title: "Fastest",
+    desc: "Prefer to emulate the highest end of hardware",
+    machine: ["q35"],
+    cpu: ["EPYC", "Skylake-Server"],
+    disk: ["nvme", "ufs", "ahci"],
   },
-  vga_options: ["virtio", "std", "qxl", "vmware", "none"]
+  "compat": {
+    index: 2,
+    title: "Compatibility",
+    desc: "Prefer to emulate the widely used hardware",
+    machine: ["pc"],
+    cpu: ["Skylake-Client", "Broadwell", "IvyBridge", "Westmere", "Penryn", "Conroe"],
+    disk: ["ide-hd", "ide-cd"],
+  },
+  "legacy": {
+    index: 3,
+    title: "Legacy",
+    desc: "Prefer to emulate ancient hardware",
+    machine: ["isapc"],
+    cpu: ["pentium3", "pentium", "486"],
+    disk: ["isa-ide", "isa-fdc", "floppy", "emmc"],
+  },
+  "exotic-old": {
+    index: 4,
+    title: "Exotic Old",
+    desc: "Prefer to emulate unique old hardware",
+    machine: ["pc", "isapc"],
+    cpu: ["athlon"],
+    disk: ["usb", "sd-card"],
+  },
+  "exotic-new": {
+    index: 5,
+    title: "Exotic New",
+    desc: "Prefer to emulate unique new hardware",
+    machine: ["q35"],
+    cpu: ["GraniteRapids", "SierraForest", "EPYC-Genoa"],
+    disk: ["am53c974", "isa-fdc", "floppy", "emmc"],
+  },
+}
+
+const x86Modes = {
+  cpu: ["host", "486", "max", "base", "EPYC", "Skylake-Server", "Skylake-Client", "Broadwell", "IvyBridge", "Westmere", "Penryn", "Conroe", "pentium3", "pentium", "athlon", "GraniteRapids", "SierraForest", "EPYC-Genoa"],
+  machine: ["q35", "pc", "isapc"],
+  disk: Object.keys({ ...commonDisks, ...x86Disks, }),
+  display: [],
+  network: [],
+  sound: [],
+  input: [],
 };
+
+const armModes = {
+  cpu: ["host", "max", "cortex-a15"],
+  machine: ["virt", "versatilepb"],
+  disk: Object.keys({ ...commonDisks }),
+  display: [],
+  network: [],
+  sound: [],
+  input: [],
+};
+
+const riscvModes = {
+  cpu: ["virt"],
+  machine: ["virt"],
+  disk: Object.keys({ ...commonDisks }),
+  display: [],
+  network: [],
+  sound: [],
+  input: [],
+};
+
+const ARCH_MATRIX = {
+  "x86_64": { binary: "qemu-system-x86_64", name: "x86 64-bit", modes: x86Modes },
+  "i386": { binary: "qemu-system-i386", name: "x86 32-bit", modes: x86Modes },
+  "aarch64": { binary: "qemu-system-aarch64", name: "ARM 64-bit", modes: armModes },
+  "arm": { binary: "qemu-system-arm", name: "ARM 32-bit", modes: armModes },
+  "riscv64": { binary: "qemu-system-riscv64", name: "RISC-V 64-bit", modes: riscvModes },
+  "riscv32": { binary: "qemu-system-riscv32", name: "RISC-V 32-bit", modes: riscvModes },
+};
+
+const OS_MATRIX = {
+  "debian": { name: "Debian/Ubuntu", vga: "virtio", net: "virtio-net-pci" },
+  "windows": { name: "Windows", vga: "std", net: "e1000", extra: ["-rtc base=localtime"] },
+  "mac": { name: "macOS", vga: "vmware", net: "virtio-net-pci", extra: ["-device isa-applesmc"] }
+}
--- main.js
+++ main.js
@@ -3,19 +3,42 @@ const { createApp, ref, computed, onMounted } = Vue;
 createApp({
     setup() {
         const saved = ref([]);
-        const form = ref({ arch: 'x86_64', os: 'debian', ram: 2, cpu: 2, vga: 'virtio' });
+        const form = ref({ arch: 'x86_64', preset: "virtualize", os: 'debian', ram: 2, smp: 2, cpu: 'max', vga: 'virtio' });
+
+        // filtered keys, decreasing order of dependency
+        const preset_keys = Object.entries(PRESET_MATRIX).map(([k, v]) => ({ key: k, i: v.index })).sort((a, b) => a.i > b.i).map(x => x.key);
+        const arch_keys = ref(Object.keys(ARCH_MATRIX));
+        const machine_keys = ref(Object.keys(MACHINES));
+        const cpu_keys = ref(Object.keys(CPUS));
+        const disk_keys = ref(Object.keys(DISKS));
+        const display_keys = ref(Object.keys({}));
+        const network_keys = ref(Object.keys({}));
+        const sound_keys = ref(Object.keys({}));
+        const input_keys = ref(Object.keys({}));
 
         const cmd = computed(() => {
-            const archInfo = configData.architectures[form.value.arch];
-            const osInfo = configData.os_presets[form.value.os];
+            const presetInfo = PRESET_MATRIX[form.value.preset];
+            const archInfo = ARCH_MATRIX[form.value.arch];
+            if (!archInfo || !presetInfo) {
+                return '// Invalid preset or arch combination';
+            }
+            cpu_keys.value = archInfo.modes.cpu;
+            machine_keys.value = archInfo.modes.machine;
+            disk_keys.value = archInfo.modes.disk;
+
+            if ([cpu_keys, machine_keys, disk_keys].some(x => x.length == 0)) {
+                return '// Data is empty here';
+            }
+
+            const osInfo = OS_MATRIX[form.value.os];
             const br = form.value.os == 'windows' ? " `\n  " : " \\\n  ";
 
             let parts = [
                 archInfo.binary,
                 `-m ${form.value.ram * 1024}`,
-                `-smp ${form.value.cpu}`,
-                `-machine ${archInfo.machine}`,
-                `-cpu ${archInfo.cpu}`,
+                `-smp ${form.value.smp}`,
+                `-machine ${form.value.machine}`,
+                `-cpu ${form.value.cpu}`,
                 `-vga ${form.value.vga}`,
                 `-netdev user,id=net0 -device ${osInfo.net},netdev=net0`
             ];
@@ -44,6 +67,10 @@ createApp({
             if (raw) saved.value = JSON.parse(raw);
         });
 
-        return { form, cmd, configData, saved, saveToLocal, loadFromLocal, removeSave };
+        return {
+            PRESET_MATRIX, ARCH_MATRIX, OS_MATRIX, MACHINES, CPUS, DISKS, DISPLAYS, NETWORKS, SOUNDS, INPUTS,
+            preset_keys, arch_keys, machine_keys, cpu_keys, disk_keys, display_keys, network_keys, sound_keys, input_keys,
+            form, cmd, saved, saveToLocal, loadFromLocal, removeSave
+        };
     }
 }).mount('#app');
